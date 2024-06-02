5c08152123486a5b113813fc4d0c6ceee593e1fb
NikolaCBS <nikola@ceresblockchain.solutions>
2024-06-02 03:54:28 +0200

    Add carbon credit batch proposal tests

--- pallets/veles/src/tests.rs
+++ pallets/veles/src/tests.rs
@@ -378,15 +378,15 @@ fn cast_vote_ccbatch_ok() {
 			penalty_timeout: 0,
 		};
 
-		// Create CC batch hash
+		// Create CC batch and project hash
 		let nonce = frame_system::Pallet::<Test>::account_nonce(bob());
 		let encoded: [u8; 32] = (bob(), nonce).using_encoded(blake2_256);
-		let batch_hash = H256::from(encoded);
+		let hash = H256::from(encoded);
 
 		// Create CC batch proposal info
 		let ccb_proposal_info = CCBProposalInfo {
-			project_hash: batch_hash,
-			batch_hash,
+			project_hash: hash,
+			batch_hash: hash,
 			creation_date: 0,
 			credit_amount: 0,
 			initial_credit_price: 0,
@@ -568,5 +568,220 @@ fn propose_project_project_proposal_already_exists() {
 			),
 			Error::<Test>::ProjectProposalAlreadyExists
 		);
+	});
+}
+
+#[test]
+fn propose_carbon_credit_batch_unauthorized() {
+	new_test_ext().execute_with(|| {
+		// Create CC batch proposal IPFS link
+		let ipfs_ccbatch_proposal_documentation =
+			BoundedString::<IPFSLength>::truncate_from("ipfs_ccbatch_proposal_documentation");
+
+		// Go past genesis block so events get deposited
+		System::set_block_number(1);
+
+		// Create project hash
+		let nonce = frame_system::Pallet::<Test>::account_nonce(bob());
+		let encoded: [u8; 32] = (bob(), nonce).using_encoded(blake2_256);
+		let hash = H256::from(encoded);
+
+		// Check for Unauthorized error
+		assert_err!(
+			Veles::propose_carbon_credit_batch(
+				RuntimeOrigin::signed(alice()),
+				hash,
+				0,
+				0,
+				ipfs_ccbatch_proposal_documentation,
+			),
+			Error::<Test>::Unauthorized
+		);
+	});
+}
+
+#[test]
+fn propose_carbon_credit_batch_project_doesnt_exist() {
+	new_test_ext().execute_with(|| {
+		// Create CC batch proposal IPFS link
+		let ipfs_ccbatch_proposal_documentation =
+			BoundedString::<IPFSLength>::truncate_from("ipfs_ccbatch_proposal_documentation");
+
+		// Create project owner info
+		let project_owner_info = PVoPOInfo {
+			documentation_ipfs: BoundedString::<IPFSLength>::truncate_from(
+				"ipfs_project_owner_documentation",
+			),
+			penalty_level: 0,
+			penalty_timeout: 0,
+		};
+
+		// Go past genesis block so events get deposited
+		System::set_block_number(1);
+
+		// Insert project owner
+		ProjectOwners::<Test>::insert(alice(), project_owner_info);
+
+		// Create project hash
+		let nonce = frame_system::Pallet::<Test>::account_nonce(bob());
+		let encoded: [u8; 32] = (bob(), nonce).using_encoded(blake2_256);
+		let hash = H256::from(encoded);
+
+		// Check for Unauthorized error
+		assert_err!(
+			Veles::propose_carbon_credit_batch(
+				RuntimeOrigin::signed(alice()),
+				hash,
+				0,
+				0,
+				ipfs_ccbatch_proposal_documentation,
+			),
+			Error::<Test>::ProjectDoesntExist
+		);
+	});
+}
+
+#[test]
+fn propose_carbon_credit_batch_unauthorized_project_owner() {
+	new_test_ext().execute_with(|| {
+		// Create CC batch proposal IPFS link
+		let ipfs_ccbatch_proposal_documentation =
+			BoundedString::<IPFSLength>::truncate_from("ipfs_ccbatch_proposal_documentation");
+		// Create project proposal IPFS link
+		let ipfs_project_proposal_documentation =
+			BoundedString::<IPFSLength>::truncate_from("ipfs_project_proposal_documentation");
+
+		// Create project owner info (ALICE)
+		let project_owner_info_1 = PVoPOInfo {
+			documentation_ipfs: BoundedString::<IPFSLength>::truncate_from(
+				"ipfs_project_owner_documentation_1",
+			),
+			penalty_level: 0,
+			penalty_timeout: 0,
+		};
+
+		// Create project owner info (BOB)
+		let project_owner_info_2 = PVoPOInfo {
+			documentation_ipfs: BoundedString::<IPFSLength>::truncate_from(
+				"ipfs_project_owner_documentation_2",
+			),
+			penalty_level: 0,
+			penalty_timeout: 0,
+		};
+
+		// Go past genesis block so events get deposited
+		System::set_block_number(1);
+
+		// Insert project owner
+		ProjectOwners::<Test>::insert(alice(), project_owner_info_1);
+		ProjectOwners::<Test>::insert(bob(), project_owner_info_2);
+
+		// Create project hash
+		let nonce = frame_system::Pallet::<Test>::account_nonce(alice());
+		let encoded: [u8; 32] = (alice(), nonce).using_encoded(blake2_256);
+		let project_hash = H256::from(encoded);
+
+		// Create project proposal info
+		let project_proposal_info = ProjectProposalInfo {
+			project_owner: alice(),
+			creation_date: 0,
+			project_hash,
+			votes_for: BTreeSet::<AccountId>::new(),
+			votes_against: BTreeSet::<AccountId>::new(),
+		};
+
+		// Insert project proposal info
+		ProjectProposals::<Test>::insert(
+			ipfs_project_proposal_documentation.clone(),
+			project_proposal_info,
+		);
+
+		// Create project info
+		let project_info = ProjectInfo {
+			documentation_ipfs: ipfs_project_proposal_documentation.clone(),
+			creation_date: 0,
+			penalty_level: 0,
+			penalty_timeout: 0,
+		};
+
+		Projects::<Test>::insert(project_hash, project_info);
+
+		// Check for Unauthorized error (Other project owner)
+		assert_err!(
+			Veles::propose_carbon_credit_batch(
+				RuntimeOrigin::signed(bob()),
+				project_hash,
+				0,
+				0,
+				ipfs_ccbatch_proposal_documentation,
+			),
+			Error::<Test>::Unauthorized
+		);
+	});
+}
+
+#[test]
+fn propose_carbon_credit_batch_ok() {
+	new_test_ext().execute_with(|| {
+		// Create CC batch proposal IPFS link
+		let ipfs_ccbatch_proposal_documentation =
+			BoundedString::<IPFSLength>::truncate_from("ipfs_ccbatch_proposal_documentation");
+		// Create project proposal IPFS link
+		let ipfs_project_proposal_documentation =
+			BoundedString::<IPFSLength>::truncate_from("ipfs_project_proposal_documentation");
+
+		// Create project owner info
+		let project_owner_info = PVoPOInfo {
+			documentation_ipfs: BoundedString::<IPFSLength>::truncate_from(
+				"ipfs_project_owner_documentation",
+			),
+			penalty_level: 0,
+			penalty_timeout: 0,
+		};
+
+		// Go past genesis block so events get deposited
+		System::set_block_number(1);
+
+		// Insert project owner
+		ProjectOwners::<Test>::insert(alice(), project_owner_info);
+
+		// Create project hash
+		let nonce = frame_system::Pallet::<Test>::account_nonce(alice());
+		let encoded: [u8; 32] = (alice(), nonce).using_encoded(blake2_256);
+		let project_hash = H256::from(encoded);
+
+		// Create project proposal info
+		let project_proposal_info = ProjectProposalInfo {
+			project_owner: alice(),
+			creation_date: 0,
+			project_hash,
+			votes_for: BTreeSet::<AccountId>::new(),
+			votes_against: BTreeSet::<AccountId>::new(),
+		};
+
+		// Insert project proposal info
+		ProjectProposals::<Test>::insert(
+			ipfs_project_proposal_documentation.clone(),
+			project_proposal_info,
+		);
+
+		// Create project info
+		let project_info = ProjectInfo {
+			documentation_ipfs: ipfs_project_proposal_documentation.clone(),
+			creation_date: 0,
+			penalty_level: 0,
+			penalty_timeout: 0,
+		};
+
+		Projects::<Test>::insert(project_hash, project_info);
+
+		// Propose carbon credit batch succesfully
+		assert_ok!(Veles::propose_carbon_credit_batch(
+			RuntimeOrigin::signed(alice()),
+			project_hash,
+			0,
+			0,
+			ipfs_ccbatch_proposal_documentation,
+		));
 	});
 }
